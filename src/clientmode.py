f22c89f2631aeca8fae76d497781510126bbc213
Matt Davis <mattdavis90@googlemail.com>
2014-07-06 01:00:29 +0100

    Backup and versioning complete

--- src/clientmode.py
+++ src/clientmode.py
@@ -1,4 +1,32 @@
 import logging
+import threading
+import re
+import xmlrpclib
+import os
+import subprocess
+from datetime import datetime, timedelta
+from socket import gethostname
+from time import sleep
+
+
+def wrap_xmlrpc(func, *args, **kwargs):
+    try:
+        return func(*args, **kwargs)
+    except xmlrpclib.Error as err:
+        logging.error('There was an XMLRPC error: %s' % err)
+    except Exception:
+        logging.error('Unable to connect to XMLRPC server')
+
+    return None
+
+def _tdelta(timestring):
+    regex = "^((?P<weeks>\d+)w ?)?((?P<days>\d+)d ?)?((?P<hours>\d+)h ?)?((?P<minutes>\d+)m ?)?((?P<seconds>\d+)s ?)?$"
+    kwargs = {}
+
+    for k, v in re.match(regex, timestring).groupdict(default="0").items():
+        kwargs[k] = int(v)
+
+    return timedelta(**kwargs)
 
 
 class ClientMode(object):
@@ -11,12 +39,88 @@ class ClientMode(object):
         if not config.has_option('client', 'server'):
             raise RuntimeError('Server not specified in config file')
 
-        self._server = config.get('client', 'server')
+        server_url = config.get('client', 'server')
+
+        regex = '^http://.*:\d+/$'
+
+        if not re.search(regex, server_url):
+            raise RuntimeError('Server should be in the format http://127.0.0.1:9001/')
+
+        self._proxy = xmlrpclib.ServerProxy(server_url)
+        self._hostname = gethostname()
+
+        self._running = threading.Event()
+        self._running.clear()
 
         logging.debug('Client checks passed')
 
     def run(self):
         logging.debug('Starting XMLRPC client')
 
+        self._running.set()
+
+        config = None
+
+        config = wrap_xmlrpc(self._proxy.get_artifacts, self._hostname)
+
+        if config:
+            artifacts = []
+            for name, properties in config.items():
+                artifacts.append(Artifact(self._proxy, self._hostname, name, **properties))
+
+            while self._running.is_set():
+                for artifact in artifacts:
+                    if artifact.ready_to_run():
+                        artifact.run()
+
+                sleep(1)
+
     def stop(self):
         logging.debug('Stopping XMLRPC client')
+
+        self._running.clear()
+
+class Artifact(object):
+    def __init__(self, proxy, hostname, name, **kwargs):
+        self._proxy = proxy
+        self._hostname = hostname
+        self._name = name
+
+        self._file_based = kwargs.get('file_based', True)
+        self._filename = kwargs.get('filename', '')
+        self._backup_command = kwargs.get('backup_command', '').split(' ')
+        self._cleanup = kwargs.get('cleanup', False)
+        self._interval = _tdelta(kwargs.get('interval', '1h'))
+        
+        self._next_run = datetime.now()
+
+        logging.info('Artifact %s - Loaded' % self._name)
+
+    def ready_to_run(self):
+        return self._next_run <= datetime.now()
+
+    def run(self):
+        logging.info('Artifact %s - Running' % self._name)
+
+        self._next_run = datetime.now() + self._interval
+
+        if not self._file_based:
+            logging.debug('Artifact %s - Running backup command: %s' % (self._name, self._backup_command))
+            
+            subprocess.call(self._backup_command, shell=True)
+
+        if os.path.isfile(self._filename):
+            logging.debug('Artifact %s - Reading file' % self._name)
+            
+            with open(self._filename, 'rb') as handle:
+                data = xmlrpclib.Binary(handle.read())
+            
+            logging.debug('Artifact %s - Sending file to XMLRPC server' % self._name)
+            
+            wrap_xmlrpc(self._proxy.store_version, self._hostname, self._name, data)
+
+            if self._cleanup:
+                logging.debug('Artifact %s - Removing file' % self._name)
+                os.unlink(self._filename)
+
+            logging.info('Artifact %s - Finished running' % self._name)
--- src/servermode.py
+++ src/servermode.py
@@ -1,7 +1,8 @@
 import logging
+import re
+import os
 from DocXMLRPCServer import DocXMLRPCServer
 from DocXMLRPCServer import DocXMLRPCRequestHandler
-import threading
 
 
 class ServerMode(object):
@@ -9,6 +10,30 @@ class ServerMode(object):
         logging.debug('Starting server mode checks on config file')
 
         self._clients = {}
+
+        self._backup_location = ''
+        self._port = 9001
+
+        if config.has_option('general', 'backup_location'):
+            self._backup_location = config.get('general', 'backup_location')
+
+            if not os.path.isdir(self._backup_location):
+                logging.warn("Backup location '%s' does not exist, attempting to create it" % self._backup_location)
+
+                try:
+                    os.makedirs(self._backup_location)
+                except:
+                    raise RuntimeError('Could not create the requested backup location')
+        else:
+            raise RuntimeError('Backup location not specified in config file')
+
+        if not config.has_option('general', 'port'):
+            logging.warn('No port specified, using 9001')
+        else:
+            try:
+                self._port = int(config.get('general', 'port'))
+            except:
+                raise RuntimeError('Server port must be an integer')
 
         for section in config.sections():
             if not section == 'general':
@@ -28,17 +53,25 @@ class ServerMode(object):
 
                     file_based = True
                     filename = ''
-                    command = ''
-                    cleanup = True
+                    backup_command = ''
+                    restore_command = ''
+                    cleanup = False
                     versions = 1
-                    interval = 3600
+                    interval = '1h'
 
-                    if not config.has_option(section, artifact + '_file'):
+                    if config.has_option(section, artifact + '_filename'):
+                        filename = config.get(section, artifact + '_filename')
+                    else:
                         raise RuntimeError("Artifacts must have at least a file specified. Error in client '%s'" % section)
 
-                    if config.has_option(section, artifact + '_command'):
+                    if config.has_option(section, artifact + '_backup_command'):
                         file_based = False
-                        command = config.get(section, artifact + '_command')
+                        backup_command = config.get(section, artifact + '_backup_command')
+
+                        if config.has_option(section, artifact + '_restore_command'):
+                            restore_command = config.get(section, artifact + '_restore_command')
+                        else:
+                            raise RuntimeError("A backup command was specified without a restore command. A restore command is required in client '%s', artifact '%s'" % (section, artifact))
 
                     if config.has_option(section, artifact + '_cleanup'):
                         tmp = config.get(section, artifact + '_cleanup')
@@ -52,20 +85,22 @@ class ServerMode(object):
 
                     if config.has_option(section, artifact + '_versions'):
                         try:
-                            version = int(config.get(section, artifact + '_versions'))
+                            versions = int(config.get(section, artifact + '_versions'))
                         except:
                             raise RuntimeError("Version option must be an integer in client '%s', artifact '%s'" % (section, artifact))
 
                     if config.has_option(section, artifact + '_interval'):
-                        try:
-                            interval = int(config.get(section, artifact + '_interval'))
-                        except:
-                            raise RuntimeError("Interval option must be an integer in client '%s', artifact '%s'" % (section, artifact))
+                        interval = config.get(section, artifact + '_interval')
+                        regex = "^(\d+w ?)?(\d+d ?)?(\d+h ?)?(\d+m ?)?(\d+s ?)?$"
+
+                        if not re.search(regex, interval):
+                            raise RuntimeError("Interval option must in valid timedelta format. e.g. 1w2d3h4m. In client '%s', artifact '%s'" % (section, artifact))
 
                     artifacts[artifact] = {
                         'file_based': file_based,
                         'filename': filename,
-                        'command': command,
+                        'backup_command': backup_command,
+                        'restore_command': restore_command,
                         'cleanup': cleanup,
                         'versions': versions,
                         'interval': interval
@@ -81,8 +116,8 @@ class ServerMode(object):
     def run(self):
         logging.debug('Starting XMLRPC server')
 
-        self._server = DocXMLRPCServer(("localhost", 8000), logRequests=False)
-        self._server.register_instance(_XMLRPCServer(self._clients))
+        self._server = DocXMLRPCServer(('0.0.0.0', self._port), logRequests=False)
+        self._server.register_instance(_XMLRPCServer(self._clients, self._backup_location))
         self._server.serve_forever()
 
     def stop(self):
@@ -93,17 +128,78 @@ class ServerMode(object):
 
 
 class _XMLRPCServer(object):
-    def __init__(self, clients):
+    def __init__(self, clients, backup_location):
         self._clients = clients
+        self._backup_location = backup_location
 
     def get_artifacts(self, client):
-        return self._clients[client]
+        logging.info('Client %s - Requested artifacts' % client)
 
-    def store_version(self, client, artifact, data):
-        pass
+        return self._clients.get(client, {})
 
+    def store_version(self, client, artifact, binary):
+        if not self._clients.get(client):
+            raise
+
+        no_versions = self._clients[client][artifact]['versions']
+         
+        backup_path = os.path.join(self._backup_location, client, artifact)
+
+        if not os.path.isdir(backup_path):
+            logging.warn("Client %s - Artifact %s - directory doesn't exist, attempting to create" % (client, artifact))
+            
+            try:
+                os.makedirs(backup_path)
+            except:
+                logging.error('Client %s - Artifact %s - Could not create directory')
+
+                raise RuntimeError('Could not create backup directory for %s artifact %s' % (client, artifact))
+
+        backup_file = os.path.join(backup_path, 'version0')
+
+        versions = self.list_versions(client, artifact)
+
+        if len(versions) >= no_versions:
+            logging.debug('Client %s - Artifact %s - Removing old version' % (client, artifact))
+
+            os.unlink(versions[-1]['filepath'])
+            
+            del versions[-1]
+
+        if len(versions) > 0:
+            for i in range(len(versions) - 1, -1, -1):
+                old_name = versions[i]['filename']
+                new_name = 'version%d' % (int(re.match('^.*(\d+)$', old_name).group(1)) + 1)
+
+                old_path = os.path.join(backup_path, old_name)
+                new_path = os.path.join(backup_path, new_name)
+
+                os.rename(old_path, new_path)
+
+        with open(backup_file, 'wb') as handle:
+            handle.write(binary.data)
+
+        logging.info('Client %s - Artifact %s - Stored new version' % (client, artifact))
+
+        return True
+        
     def get_version(self, client, artifact, version):
         pass
 
     def list_versions(self, client, artifact):
-        pass
+        backup_path = os.path.join(self._backup_location, client, artifact)
+
+        versions = []
+
+        if os.path.isdir(backup_path):
+            backup_list = os.listdir(backup_path)
+
+            for backup in backup_list:
+                file_path = os.path.join(backup_path, backup)
+                modified = os.stat(file_path).st_mtime
+
+                versions.append({'filename': backup, 'filepath': file_path, 'modified': modified})
+
+            versions.sort(key=lambda itm: itm['modified'], reverse=True)
+
+        return versions
